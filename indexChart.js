b0f14b9cf47038c4d19d7305436e40e9d97329b3
Alexis Montes <montes.alexis25@gmail.com>
2022-01-10 01:56:29 -0500

    fixed colors labeled user story solutions

--- indexChart.js
+++ indexChart.js
@@ -1,7 +1,7 @@
 const width = 1200;
 const height = 500;
 const margin = { top: 100, right: 100, bottom: 100, left: 100 };
-const color = ['#C0504D', '#388D38']
+const color = [ '#91C7B1', '#E63462']
 
 let JSON_URL = 'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json';
 
@@ -18,18 +18,19 @@ d3.json(JSON_URL).then((data) => {
         d.Time = new Date(1970,0,1,0,parsedTime[0],parsedTime[1])
     })
 
+    // user story 7,8 creates ranges baed on the data
     let xScale = d3.scaleLinear()
         .domain(d3.extent(data, d => d.Year ))
-        .range([margin.left, width - margin.right]);
+        .range([margin.left, width - margin.right]) // user story 11
 
     let yScale = d3.scaleTime()
         .domain(d3.extent(data, d => d.Time ))
-        .range([height - margin.bottom, margin.top]);
+        .range([height - margin.bottom, margin.top]); // user story 12
 
     let xAxis = d3.axisBottom(xScale)
-        .tickFormat(d3.format("d"));
+        .tickFormat(d3.format("d")); // user story 10
     let yAxis = d3.axisLeft(yScale)
-        .tickFormat(d3.timeFormat("%M:%S"));
+        .tickFormat(d3.timeFormat("%M:%S")); // user story 9
 
     let legendContainer = svg.append('g')
         .attr('id', 'legend')
@@ -39,6 +40,7 @@ d3.json(JSON_URL).then((data) => {
         .enter()
         .append('g')
         .attr('transform', (d, i) => 'translate(0, ' + (height/2 - i * 20) + ')')
+        .attr('id', 'legend') // user story 13
 
     legend.append('rect')
         .attr('x', width - margin.right - 20)
@@ -51,26 +53,34 @@ d3.json(JSON_URL).then((data) => {
         .attr('y', 10)
         .attr('dy', '.35em')
         .style('text-anchor', 'end')
-        .text((c) => c !== '#388D38' ? 'Riders with doping allegations' : 'No doping allegations');
+        .text((c) => c !== color[0] ? 'Riders with doping allegations' : 'No doping allegations');
 
 
     svg.append("g")
         .attr("transform", `translate(0, ${height - margin.bottom})`)
-        .call(xAxis);
+        .call(xAxis)
+        .attr("id", "x-axis"); // user story 2
 
     svg.append("g")
         .attr("transform", `translate(${margin.left}, 0)`)
-        .call(yAxis);
+        .call(yAxis)
+        .attr("id", "y-axis"); // user story 3
 
     svg.selectAll("circle")
         .data(data)
         .enter()
         .append("circle")
-        .attr("cx", d => xScale(d.Year))
-        .attr("cy", d => yScale(d.Time))
+        .attr("cx", d => xScale(d.Year)) // user story 11
+        .attr("cy", d => yScale(d.Time)) // user story 12
         .attr("r", 5)
-        .attr("class", "dot")
-        .attr("fill", (d) => d.Doping ? '#388D38' : '#C0504D');
+        .attr("class", "dot") // user story 4
+        .attr("fill", (d) => d.Doping ? color[0] : color[1])
+        .attr("data-xvalue", d => d.Year) // user story 5 part 1
+        .attr("data-yvalue", d => d.Time) // user story 5 part 2
+        .append("title")
+        .text( (d) => `Name: ${d.Name}\nNationality: ${d.Nationality}\nDoping Charge: ${d.Doping ? d.Doping: 'None'} `)
+        .attr('id', 'tooltip') // user story 14
+        .attr('data-year', (d) => d.Year) // user story 15
 
 
 
